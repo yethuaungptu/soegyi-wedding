83bf90b41e6b164abd47140da4fb57575063bc0e
yethuaungptu <yethuaung481995@gmail.com>
2023-07-24 22:36:57 +0630

    Invitation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var url = require("url");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/invit", function (req, res) {
+  var q = url.parse(req.url, true).query;
+  res.render("invitation", { name: q.u || "unknown" });
 });
 
 module.exports = router;
